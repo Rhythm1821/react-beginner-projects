26338ec9c72193fbbe18abeca69febc9c3fb8622
Rhythm1821 <alamrawat1821@gmail.com>
2024-11-18 22:50:04 +0530

    newsflow

---
+++ newsflow/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import NewsBoard from './components/NewsBoard'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <NewsBoard />
+    </>
+  )
+}
+
+export default App
---
+++ newsflow/src/components/NewsBoard.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import NewsItem from "./NewsItem"
+
+export default function NewsBoard() {
+    const [articles, setArticles] = useState([])
+
+    useEffect(()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${import.meta.env.VITE_API_KEY}`
+        fetch(url)
+            .then((res)=>res.json())
+            .then((data)=> setArticles(data.articles))
+    },[])
+    return (
+        <>
+            <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+            {
+                articles.map((article,index)=>(
+                    <NewsItem key={index} article={article} title={article.title} description={article.description} src={article.urlToImage} url={article.url} />
+                ))
+            }
+        </>
+    )
+}
